8e5a91c914043ed1b6943bf2d9c355ba05c926c5
Tom Esterez <testerez@gmail.com>
2019-09-13 21:32:13 -0400

    Refacor useEventListener

--- packages/flame/src/hooks/useEventListener.ts
+++ packages/flame/src/hooks/useEventListener.ts
@@ -5,21 +5,18 @@ type CallbackEvent<T> = (event: T) => void;
 export function useEventListener<T extends Event>(
   eventKey: string,
   callback: CallbackEvent<T>,
-  target?: EventTarget,
+  target: EventTarget = document,
 ) {
   React.useEffect(() => {
     const captureEvent: CallbackEvent<T> = event => {
       callback(event);
     };
 
-    if (target || document) {
-      (target || document).addEventListener(eventKey, captureEvent as any);
+    if (target) {
+      target.addEventListener(eventKey, captureEvent as any);
+      return () => {
+        target.removeEventListener(eventKey, captureEvent as any);
+      };
     }
-
-    return () => {
-      if (target || document) {
-        (target || document).removeEventListener(eventKey, captureEvent as any);
-      }
-    };
   }, [callback]);
 }
