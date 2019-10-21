5809a803ff10914d8cfd3b59c23756f4c0f77bc5
Xavier Drdak <1198051+xdrdak@users.noreply.github.com>
2019-10-22 04:55:26 +0900

    Mobile friendly Alert & Icon (#27)
    * Make Alert and Icon mobile friendlier

--- packages/flame/src/Alert/Alert.tsx
+++ packages/flame/src/Alert/Alert.tsx
@@ -1,49 +1,56 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
 import { transparentize } from 'polished';
-import { space, variant, SpaceProps } from 'styled-system';
+import { space, variant, SpaceProps, layout, LayoutProps } from 'styled-system';
 import { themeGet } from '@styled-system/theme-get';
 import { Flex, Box } from '../Core';
 import { Text } from '../Text';
+
+const borderTopHeight = '4px';
 
 const alertStyles = variant({
   key: 'alertVariants',
   prop: 'type',
 });
 
-const AlertWrapper = styled('div')<{ type: string }>`
+const AlertWrapper = styled('div')<SpaceProps & { type: string }>`
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
   box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.06), 0 3px 6px 0 rgba(0, 0, 0, 0.03),
     0 1px 2px 0 rgba(0, 0, 0, 0.1);
-  border-top: 4px solid;
+  border-top: ${borderTopHeight} solid;
   border-radius: ${themeGet('radii.radius-2')};
-  padding: ${themeGet('space.2')} ${themeGet('space.3')};
   ${space}
   ${alertStyles}
 `;
 
 AlertWrapper.defaultProps = {
   type: 'info',
+  px: 3,
+  py: [3, 2],
 };
 
-const CloseButton = styled('button')`
+const CloseButton = styled('button')<LayoutProps>`
   font-size: ${themeGet('fontSizes.text')};
   color: ${themeGet('colors.textHeading')};
   background-color: ${props => transparentize(0.9, themeGet('colors.textHeading', '#000')(props))};
   border-radius: 50%;
   border: none;
   cursor: pointer;
-  width: 1em;
-  height: 1em;
   padding: 0;
   position: relative;
+  ${layout}
 
   &:focus {
     outline: none;
   }
 `;
+
+CloseButton.defaultProps = {
+  width: ['18px', '16px'],
+  height: ['18px', '16px'],
+};
 
 CloseButton.displayName = 'CloseButton';
 
@@ -82,46 +89,59 @@ const Alert: React.FunctionComponent<AlertProps & SpaceProps> = ({
 
   return (
     <AlertWrapper type={type} {...restProps}>
-      <Flex flex="1" alignItems="flex-start">
+      <Flex flex="1" alignItems="flex-start" mt={`-${borderTopHeight}`}>
         {icon && (
-          <Flex alignItems="center" pr={2} pt="2px">
+          <Box lineHeight={4} pr={2}>
             {icon}
-          </Flex>
+          </Box>
         )}
         <Box flex="1">
-          {title && (
-            <Text color="textHeading" fontWeight="bold" fontSize="text" mt={0} mr={0} mb={1} ml={0}>
-              {title}
-            </Text>
-          )}
-          <Box fontSize="text-s">{children}</Box>
+          <Flex alignItems="center">
+            {title && (
+              <Box flex="1">
+                <Text
+                  color="textHeading"
+                  fontWeight="bold"
+                  lineHeight={4}
+                  fontSize={['text-l', 'text']}
+                  mt={0}
+                  mr={0}
+                  mb={0}
+                  ml={0}
+                >
+                  {title}
+                </Text>
+              </Box>
+            )}
+            {!noCloseBtn && (
+              <CloseButton type="button" onClick={handleClose}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 8 8"
+                  css={{
+                    width: '0.5em',
+                    height: '0.5em',
+                    fill: 'black',
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                  }}
+                >
+                  <g fillRule="evenodd" transform="translate(-4 -4)">
+                    <path
+                      fillOpacity=".5"
+                      d="M9.414 8l2.122-2.121a1 1 0 1 0-1.415-1.415L8 6.586 5.879 4.464A1 1 0 0 0 4.464 5.88L6.586 8l-2.122 2.121a1 1 0 0 0 1.415 1.415L8 9.414l2.121 2.122a1 1 0 0 0 1.415-1.415L9.414 8z"
+                    />
+                  </g>
+                </svg>
+              </CloseButton>
+            )}
+          </Flex>
+
+          <Box fontSize={['text', 'text-s']}>{children}</Box>
         </Box>
       </Flex>
-      {!noCloseBtn && (
-        <CloseButton type="button" onClick={handleClose}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 8 8"
-            css={{
-              width: '0.5em',
-              height: '0.5em',
-              fill: 'black',
-              position: 'absolute',
-              top: '0.25em',
-              left: '0.25em',
-              right: '0',
-              bottom: '0',
-            }}
-          >
-            <g fillRule="evenodd" transform="translate(-4 -4)">
-              <path
-                fillOpacity=".5"
-                d="M9.414 8l2.122-2.121a1 1 0 1 0-1.415-1.415L8 6.586 5.879 4.464A1 1 0 0 0 4.464 5.88L6.586 8l-2.122 2.121a1 1 0 0 0 1.415 1.415L8 9.414l2.121 2.122a1 1 0 0 0 1.415-1.415L9.414 8z"
-              />
-            </g>
-          </svg>
-        </CloseButton>
-      )}
     </AlertWrapper>
   );
 };
