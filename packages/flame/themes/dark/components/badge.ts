d42f655387a80bba0a0c9568c0b372384629cc8c
Alexis Morin <alexis.morin@lightspeedhq.com>
2019-11-11 15:38:51 -0500

    badge

--- packages/flame/themes/dark/components/badge.ts
+++ packages/flame/themes/dark/components/badge.ts
@@ -2,28 +2,28 @@ import { colors } from '../colors';
 
 const badgeVariants: BadgeVariants = {
   default: {
-    backgroundColor: colors['gray-100'],
+    backgroundColor: colors['gray-300'],
     color: colors['gray-900'],
   },
   success: {
-    backgroundColor: colors['green-700'],
-    color: colors.white,
+    backgroundColor: colors.primary,
+    color: colors['gray-900'],
   },
   warning: {
-    backgroundColor: colors['orange-500'],
+    backgroundColor: colors['orange-300'],
     color: colors['gray-900'],
   },
   danger: {
-    backgroundColor: colors['maple-500'],
-    color: colors.white,
+    backgroundColor: colors.danger,
+    color: colors.bodyBg,
   },
   info: {
-    backgroundColor: colors['blue-500'],
-    color: colors.white,
+    backgroundColor: colors.secondary,
+    color: colors.bodyBg,
   },
   important: {
-    backgroundColor: colors['blue-500'],
-    color: colors.white,
+    backgroundColor: colors.secondary,
+    color: colors.bodyBg,
   },
 };
 
