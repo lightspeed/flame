0db09e463a2a168f5e985861435a0b5ca66c2589
Xavier Drdak <1198051+xdrdak@users.noreply.github.com>
2019-11-26 13:16:04 -0500

    Make select more mobile friendly (#52)
    * Make select more mobile friendly
    
    * Add Percy breakpoints to select story
    
    * Fix border radii for select
    
    * Update select snapshot

--- packages/flame/src/Select/Select.tsx
+++ packages/flame/src/Select/Select.tsx
@@ -1,56 +1,74 @@
+import * as React from 'react';
 import styled from '@emotion/styled';
 import { layout, LayoutProps, compose } from 'styled-system';
-import { themeGet } from '@styled-system/theme-get';
-import { border, BorderProps } from '../Core';
+import { border, BorderProps, css, themeGet } from '../Core';
 
 export type SelectProps = React.SelectHTMLAttributes<HTMLSelectElement>;
 
-export const Select = styled('select')<Omit<LayoutProps, 'size'> & BorderProps>`
+interface BaseSelectProps extends Omit<LayoutProps, 'size'>, BorderProps {}
+const BaseSelect = styled('select')<BaseSelectProps>`
   width: 100%;
-  color: ${themeGet('selectStyles.color')};
-  font-size: ${themeGet('fontSizes.text-s')};
-  font-family: ${themeGet('fontFamily.sans-serif')};
-  font-weight: ${themeGet('fontWeights.bold')};
-  height: ${themeGet('space.6')};
-  padding: 0 ${themeGet('space.4')} 0 ${themeGet('space.2')};
-  background-color: ${themeGet('selectStyles.backgroundColor')};
   border: 1px solid ${themeGet('selectStyles.border')};
   border-radius: ${themeGet('radii.radius-1')};
-  line-height: ${themeGet('space.3')};
-  /* stylelint-disable declaration-colon-newline-after  */
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' width='16' height='16' fill='%23494c4c'%3E%3Cg id='chevron-up-down'%3E%3Cpath id='base-1' d='M8 12l2-2 1 1-3 3-3-3 1-1 2 2zm0-8L6 6 5 5l3-3 3 3-1 1-2-2z'/%3E%3C/g%3E%3C/svg%3E"),
-    ${themeGet('selectStyles.background')};
-  /* stylelint-enable */
-  appearance: none;
-  background-repeat: no-repeat;
-  background-position: right ${themeGet('space.1')} center, center;
-  z-index: 2;
 
-  &:focus {
-    outline: none;
-    border-color: ${themeGet('selectStyles.focusBorder')};
-  }
-
-  &:disabled {
-    color: ${themeGet('selectStyles.disabledColor')};
-    background-color: ${themeGet('selectStyles.disabledBackground')};
-    box-shadow: none;
-    opacity: 0.5;
-  }
-
-  /* IE 11 fix to remove default dropdown arrow */
-  &::-ms-expand {
-    display: none;
-  }
-
-  /* Firefox fix to remove inner outline on focus
-  // https://twitter.com/sarasoueidan/status/720890849748480002 */
-  &:-moz-focusring {
-    color: transparent;
-    text-shadow: 0 0 0 ${themeGet('selectStyles.color')};
-  }
   ${compose(
     layout,
     border,
   )}
 `;
+
+const generateDataImage = (size: number) => {
+  return `"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' width='${size}' height='${size}' fill='%23494c4c'%3E%3Cg id='chevron-up-down'%3E%3Cpath id='base-1' d='M8 12l2-2 1 1-3 3-3-3 1-1 2 2zm0-8L6 6 5 5l3-3 3 3-1 1-2-2z'/%3E%3C/g%3E%3C/svg%3E"`;
+};
+
+interface Props extends React.SelectHTMLAttributes<HTMLSelectElement>, BaseSelectProps {}
+export const Select: React.FC<Props> = ({ children, ...restProps }) => {
+  return (
+    <BaseSelect
+      css={css(get => ({
+        color: get('selectStyles.color'),
+        fontSize: ['text-l', 'text-s'],
+        fontFamily: 'sans-serif',
+        fontWeight: 'bold',
+        height: [get('space.8'), get('space.6')],
+        backgroundColor: get('selectStyles.backgroundColor'),
+        lineHeight: get('space.3'),
+        backgroundImage: [
+          `url(${generateDataImage(24)}), ${get('selectStyles.background')}`,
+          `url(${generateDataImage(16)}), ${get('selectStyles.background')}`,
+        ],
+        appearance: 'none',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: `right ${get('space.1')} center, center`,
+        zIndex: 2,
+        pt: 0,
+        pr: 4,
+        pb: 0,
+        pl: 2,
+        '&:focus': {
+          outline: 'none',
+          borderColor: get('selectStyles.focusBorder'),
+        },
+        '&:disabled': {
+          color: get('selectStyles.disabledColor'),
+          backgroundColor: get('selectStyles.disabledBackground'),
+          boxShadow: 'none',
+          opacity: 0.5,
+        },
+        /* IE 11 fix to remove default dropdown arrow */
+        '&::-ms-expand': {
+          display: 'none',
+        },
+        /* Firefox fix to remove inner outline on focus
+        // https://twitter.com/sarasoueidan/status/720890849748480002 */
+        '&:-moz-focusring': {
+          color: 'transparent',
+          textShadow: ` 0 0 0 ${get('selectStyles.color')}`,
+        },
+      }))}
+      {...restProps}
+    >
+      {children}
+    </BaseSelect>
+  );
+};
--- packages/flame/src/Select/story.tsx
+++ packages/flame/src/Select/story.tsx
@@ -7,41 +7,46 @@ import { Box } from '../Core';
 
 import SelectExample from './examples';
 import Readme from './README.md';
+import { percyBreakpoints } from '../../../../stories/helpers/percy';
 
 const stories = storiesOf('Select', module).addDecorator(withReadme(Readme));
 
-stories.add('Story', () => (
-  <TextContent>
-    <Heading3>Default</Heading3>
-    <Box mb={1}>
-      <SelectExample>
-        <option value="1">Option 1</option>
-        <option value="2">Option 2</option>
-        <option value="3">Option 3</option>
-      </SelectExample>
-    </Box>
-    <Heading3>Option variants</Heading3>
-    <Box mb={1}>
-      <SelectExample>
-        <option value="1">Option 1</option>
-        <option value="2" disabled>
-          Option 2 (disabled)
-        </option>
-        <option value="3">Option 3</option>
-        <optgroup label="Option group">
-          <option value="4">Option 4</option>
-          <option value="5">Option 5</option>
-          <option value="6">Option 6</option>
-        </optgroup>
-      </SelectExample>
-    </Box>
-    <Heading3>Disabled</Heading3>
-    <Box mb={1}>
-      <SelectExample disabled>
-        <option value="1">Option 1</option>
-        <option value="2">Option 2</option>
-        <option value="3">Option 3</option>
-      </SelectExample>
-    </Box>
-  </TextContent>
-));
+stories.add(
+  'Story',
+  () => (
+    <TextContent>
+      <Heading3>Default</Heading3>
+      <Box mb={1}>
+        <SelectExample>
+          <option value="1">Option 1</option>
+          <option value="2">Option 2</option>
+          <option value="3">Option 3</option>
+        </SelectExample>
+      </Box>
+      <Heading3>Option variants</Heading3>
+      <Box mb={1}>
+        <SelectExample>
+          <option value="1">Option 1</option>
+          <option value="2" disabled>
+            Option 2 (disabled)
+          </option>
+          <option value="3">Option 3</option>
+          <optgroup label="Option group">
+            <option value="4">Option 4</option>
+            <option value="5">Option 5</option>
+            <option value="6">Option 6</option>
+          </optgroup>
+        </SelectExample>
+      </Box>
+      <Heading3>Disabled</Heading3>
+      <Box mb={1}>
+        <SelectExample disabled>
+          <option value="1">Option 1</option>
+          <option value="2">Option 2</option>
+          <option value="3">Option 3</option>
+        </SelectExample>
+      </Box>
+    </TextContent>
+  ),
+  { ...percyBreakpoints },
+);
