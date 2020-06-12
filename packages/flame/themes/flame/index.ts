25fff741d44f8adb886e325503e8d587b24c382a
Xavier Drdak <1198051+xdrdak@users.noreply.github.com>
2020-06-12 16:04:17 -0400

    Toaster component (#86)
    * Add toaster component WIP
    
    * Update jest and testing-library. Tests for toaster
    
    * Update toaster readme
    
    * remove dangling TODO from toaster
    
    * Add better responsiveness for Toaster
    
    * Restrict Toaster types. Bypass click events for ToasterContainer
    
    * Update changelog. Add notice that we are using an external lib
    
    * Remove unused typing file
    
    * Spread rest on Toaster component to auto-pause timer
    
    * Update toaster behaviour to match specs
    
    * Supress warnings for logs. Fix toaster example setup
    
    * less jarring height transition
    
    * fixing text alignment
    
    * Set minHeight for container at 60px
    
    * Test out Percy with animations
    
    Co-authored-by: maartenafink <maartenafink@gmail.com>
    Co-authored-by: Guillaume Lambert <24464151+glambert@users.noreply.github.com>

--- packages/flame/themes/flame/index.ts
+++ packages/flame/themes/flame/index.ts
@@ -25,6 +25,7 @@ import { wordmarkStyles } from './components/wordmark';
 import { tableStyles } from './components/table';
 import { sidebarStyles } from './components/sidebar';
 import { npsStyles } from './components/nps';
+import { toasterVariants } from './components/toaster';
 
 const themeUI = {
   ...lightspeedTheme,
@@ -59,6 +60,7 @@ const theme: FlameUI = {
   tableStyles,
   sidebarStyles,
   npsStyles,
+  toasterVariants,
 };
 
 export { themeUI, theme };
