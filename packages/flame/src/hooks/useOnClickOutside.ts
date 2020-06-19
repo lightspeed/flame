315713070b4e194e82ce0a92ba16e3023d30a093
Xavier Drdak <1198051+xdrdak@users.noreply.github.com>
2020-06-19 14:45:23 -0400

    Force Update active poppers. Fix clickOutside handler (#105)
    * add forceUpdate to Dropdown and Popover
    
    * Replace forceUpdate with update. Fix clickOutside hook
    
    * Update CHANGELOG.md

--- packages/flame/src/hooks/useOnClickOutside.ts
+++ packages/flame/src/hooks/useOnClickOutside.ts
@@ -1,13 +1,19 @@
 import * as React from 'react';
 
 export function useOnClickOutside(ref: any, handler: (event: Event) => void) {
+  const savedCallback = React.useRef(handler);
+
+  React.useEffect(() => {
+    savedCallback.current = handler;
+  }, [handler]);
+
   React.useEffect(() => {
     const listener = (event: Event) => {
       // Do nothing if clicking ref's element or descendent elements
       if (!ref.current || ref.current.contains(event.target)) {
         return;
       }
-      handler(event);
+      savedCallback.current(event);
     };
 
     document.addEventListener('click', listener);
@@ -17,5 +23,5 @@ export function useOnClickOutside(ref: any, handler: (event: Event) => void) {
       document.removeEventListener('click', listener);
       document.removeEventListener('touchstart', listener);
     };
-  }, [ref, handler]);
+  }, [ref]);
 }
