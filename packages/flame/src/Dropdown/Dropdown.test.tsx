c2d96d841871fc6b8fc2b62d865f0e67699b580a
Xavier Drdak <1198051+xdrdak@users.noreply.github.com>
2020-07-24 13:23:37 -0400

    Fix tests for Dropdown

--- packages/flame/src/Dropdown/Dropdown.test.tsx
+++ packages/flame/src/Dropdown/Dropdown.test.tsx
@@ -1,140 +1,150 @@
 import * as React from 'react';
-import { customRender, fireEvent, act } from 'test-utils';
+import { customRender, fireEvent, act, screen } from 'test-utils';
 
 import { Dropdown, DropdownContent, useDropdown } from './Dropdown';
 
 describe('<Dropdown />', () => {
-  it('should render a button with out text content', () => {
-    const { queryByText } = customRender(
-      <Dropdown buttonContent="My Dropdown">
-        <DropdownContent>Some dropdown content</DropdownContent>
-      </Dropdown>,
-    );
+  it('should render a button with out text content', async () => {
+    await act(async () => {
+      customRender(
+        <Dropdown buttonContent="My Dropdown">
+          <DropdownContent>Some dropdown content</DropdownContent>
+        </Dropdown>,
+      );
+    });
 
-    expect(queryByText('My Dropdown')).toBeTruthy();
-    expect(queryByText('Some dropdown content')).not.toBeVisible();
+    expect(screen.queryByText('My Dropdown')).toBeTruthy();
+    expect(screen.queryByText('Some dropdown content')).not.toBeVisible();
   });
 
   it('should reveal our dropdown content when we click on the button', async () => {
-    const { queryByText } = customRender(
-      <Dropdown buttonContent="My Dropdown">Some dropdown content</Dropdown>,
-    );
-
-    expect(queryByText('My Dropdown')).toBeTruthy();
-    expect(queryByText('Some dropdown content')).not.toBeVisible();
-
-    act(() => {
-      fireEvent.click(queryByText('My Dropdown'));
+    await act(async () => {
+      customRender(<Dropdown buttonContent="My Dropdown">Some dropdown content</Dropdown>);
     });
 
-    expect(queryByText('Some dropdown content')).toBeVisible();
+    expect(screen.queryByText('My Dropdown')).toBeTruthy();
+    expect(screen.queryByText('Some dropdown content')).not.toBeVisible();
+
+    fireEvent.click(screen.queryByText('My Dropdown'));
+
+    expect(screen.queryByText('Some dropdown content')).toBeVisible();
   });
 
-  it('should close when we click outside the button', () => {
-    const { queryByText } = customRender(
-      <div>
-        <Dropdown buttonContent="My Dropdown">Some dropdown content</Dropdown>
-        <div>I am outside the dropdown</div>
-      </div>,
-    );
-
-    expect(queryByText('My Dropdown')).toBeTruthy();
-    expect(queryByText('Some dropdown content')).not.toBeVisible();
-
-    act(() => {
-      fireEvent.click(queryByText('My Dropdown'));
+  it('should close when we click outside the button', async () => {
+    await act(async () => {
+      customRender(
+        <div>
+          <Dropdown buttonContent="My Dropdown">Some dropdown content</Dropdown>
+          <div>I am outside the dropdown</div>
+        </div>,
+      );
     });
-    expect(queryByText('Some dropdown content')).toBeVisible();
 
-    act(() => {
-      fireEvent.click(queryByText('I am outside the dropdown'));
+    expect(screen.queryByText('My Dropdown')).toBeTruthy();
+    expect(screen.queryByText('Some dropdown content')).not.toBeVisible();
+
+    await act(async () => {
+      fireEvent.click(screen.queryByText('My Dropdown'));
     });
-    expect(queryByText('Some dropdown content')).not.toBeVisible();
+    expect(screen.queryByText('Some dropdown content')).toBeVisible();
+
+    await act(async () => {
+      fireEvent.click(screen.queryByText('I am outside the dropdown'));
+    });
+    expect(screen.queryByText('Some dropdown content')).not.toBeVisible();
   });
 
-  it('should trigger our custom callback when we click the button', () => {
+  it('should trigger our custom callback when we click the button', async () => {
     const mockFn = jest.fn();
-    const { queryByText } = customRender(
-      <div>
-        <Dropdown
-          buttonContent="My Dropdown"
-          onClick={toggle => {
-            mockFn();
-            toggle();
-          }}
-        >
-          Some dropdown content
-        </Dropdown>
-        <div>I am outside the dropdown</div>
-      </div>,
-    );
-
-    expect(queryByText('My Dropdown')).toBeTruthy();
-    expect(queryByText('Some dropdown content')).not.toBeVisible();
-
-    act(() => {
-      fireEvent.click(queryByText('My Dropdown'));
+    await act(async () => {
+      customRender(
+        <div>
+          <Dropdown
+            buttonContent="My Dropdown"
+            onClick={toggle => {
+              mockFn();
+              toggle();
+            }}
+          >
+            Some dropdown content
+          </Dropdown>
+          <div>I am outside the dropdown</div>
+        </div>,
+      );
     });
-    expect(queryByText('Some dropdown content')).toBeVisible();
+
+    expect(screen.queryByText('My Dropdown')).toBeTruthy();
+    expect(screen.queryByText('Some dropdown content')).not.toBeVisible();
+
+    await act(async () => {
+      fireEvent.click(screen.queryByText('My Dropdown'));
+    });
+    expect(screen.queryByText('Some dropdown content')).toBeVisible();
 
     expect(mockFn).toHaveBeenCalledTimes(1);
 
-    act(() => {
-      fireEvent.click(queryByText('I am outside the dropdown'));
+    await act(async () => {
+      fireEvent.click(screen.queryByText('I am outside the dropdown'));
     });
-    expect(queryByText('Some dropdown content')).not.toBeVisible();
+    expect(screen.queryByText('Some dropdown content')).not.toBeVisible();
 
     expect(mockFn).toHaveBeenCalledTimes(1);
   });
 
-  it('should close when we hit escape', () => {
-    const { queryByText, container } = customRender(
-      <div>
-        <Dropdown buttonContent="My Dropdown">Some dropdown content</Dropdown>
-        <div>I am outside the dropdown</div>
-      </div>,
-    );
-
-    expect(queryByText('My Dropdown')).toBeTruthy();
-    expect(queryByText('Some dropdown content')).not.toBeVisible();
-
-    act(() => {
-      fireEvent.click(queryByText('My Dropdown'));
+  it('should close when we hit escape', async () => {
+    let componentContainer: any;
+    await act(async () => {
+      const { container } = customRender(
+        <div>
+          <Dropdown buttonContent="My Dropdown">Some dropdown content</Dropdown>
+          <div>I am outside the dropdown</div>
+        </div>,
+      );
+      componentContainer = container;
     });
-    expect(queryByText('Some dropdown content')).toBeVisible();
 
-    act(() => {
-      fireEvent.keyUp(container, { key: 'Escape', code: 'Escape' });
+    expect(screen.queryByText('My Dropdown')).toBeTruthy();
+    expect(screen.queryByText('Some dropdown content')).not.toBeVisible();
+
+    await act(async () => {
+      fireEvent.click(screen.queryByText('My Dropdown'));
     });
-    expect(queryByText('Some dropdown content')).not.toBeVisible();
+    expect(screen.queryByText('Some dropdown content')).toBeVisible();
+
+    await act(async () => {
+      fireEvent.keyUp(componentContainer, { key: 'Escape', code: 'Escape' });
+    });
+    expect(screen.queryByText('Some dropdown content')).not.toBeVisible();
   });
 
-  it('should pass a close function when using a child function', () => {
-    const { queryByText } = customRender(
-      <Dropdown buttonContent="My Dropdown">
-        {(close: any) => (
-          <button type="button" onClick={close}>
-            Close Button
-          </button>
-        )}
-      </Dropdown>,
-    );
-
-    expect(queryByText('My Dropdown')).toBeTruthy();
-    expect(queryByText('Close Button')).not.toBeVisible();
-
-    act(() => {
-      fireEvent.click(queryByText('My Dropdown'));
+  it('should pass a close function when using a child function', async () => {
+    await act(async () => {
+      customRender(
+        <Dropdown buttonContent="My Dropdown">
+          {(close: any) => (
+            <button type="button" onClick={close}>
+              Close Button
+            </button>
+          )}
+        </Dropdown>,
+      );
     });
-    expect(queryByText('Close Button')).toBeVisible();
 
-    act(() => {
-      fireEvent.click(queryByText('Close Button'));
+    expect(screen.queryByText('My Dropdown')).toBeTruthy();
+    expect(screen.queryByText('Close Button')).not.toBeVisible();
+
+    await act(async () => {
+      fireEvent.click(screen.queryByText('My Dropdown'));
     });
-    expect(queryByText('Close Button')).not.toBeVisible();
+    expect(screen.queryByText('Close Button')).toBeVisible();
+
+    await act(async () => {
+      fireEvent.click(screen.queryByText('Close Button'));
+    });
+    expect(screen.queryByText('Close Button')).not.toBeVisible();
   });
 
-  it('should close the dropdown via hooks', () => {
+  it('should close the dropdown via hooks', async () => {
     const SomeButton = () => {
       const { closeDropdown } = useDropdown();
       return (
@@ -143,23 +153,25 @@ describe('<Dropdown />', () => {
         </button>
       );
     };
-    const { queryByText } = customRender(
-      <Dropdown buttonContent="My Dropdown">
-        <SomeButton />
-      </Dropdown>,
-    );
-
-    expect(queryByText('My Dropdown')).toBeTruthy();
-    expect(queryByText('Close Button')).not.toBeVisible();
-
-    act(() => {
-      fireEvent.click(queryByText('My Dropdown'));
+    await act(async () => {
+      customRender(
+        <Dropdown buttonContent="My Dropdown">
+          <SomeButton />
+        </Dropdown>,
+      );
     });
-    expect(queryByText('Close Button')).toBeVisible();
 
-    act(() => {
-      fireEvent.click(queryByText('Close Button'));
+    expect(screen.queryByText('My Dropdown')).toBeTruthy();
+    expect(screen.queryByText('Close Button')).not.toBeVisible();
+
+    await act(async () => {
+      fireEvent.click(screen.queryByText('My Dropdown'));
     });
-    expect(queryByText('Close Button')).not.toBeVisible();
+    expect(screen.queryByText('Close Button')).toBeVisible();
+
+    await act(async () => {
+      fireEvent.click(screen.queryByText('Close Button'));
+    });
+    expect(screen.queryByText('Close Button')).not.toBeVisible();
   });
 });
