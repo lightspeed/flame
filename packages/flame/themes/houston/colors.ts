492e63a2570d537328a60bf8dbb17e5a10793a5b
Sebastien Rothlisberger <sebastien@lightspeedhq.com>
2021-11-26 14:36:06 -0500

    Update colors

--- packages/flame/themes/houston/colors.ts
+++ packages/flame/themes/houston/colors.ts
@@ -3,7 +3,12 @@ import { houstonTheme } from '@lightspeed/flame-tokens';
 const themeColors = houstonTheme.colors;
 const textColor = '#27252A';
 
-const aliases: ColorAliases = {
+type HoustonColorAliases = { go: string; no: string; supplementary: string };
+
+const aliases: ColorAliases & HoustonColorAliases = {
+  go: 'var(--vd-colour--go)',
+  no: 'var(--vd-colour--no)',
+  supplementary: 'var(--vd-colour--supplementary)',
   primary: themeColors['green-500'],
   secondary: themeColors['blue-500'],
   danger: themeColors['maple-500'],
@@ -11,7 +16,7 @@ const aliases: ColorAliases = {
   white: themeColors.white,
   textBody: textColor,
   textHeading: textColor,
-  textDimmed: themeColors['gray-600'],
+  textDimmed: themeColors['gray-400'],
   dimmed: themeColors['gray-50'],
   disabled: themeColors['gray-100'],
   bodyBg: themeColors['gray-50'],
